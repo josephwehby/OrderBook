891b06fc42ddabff3add23223554c33911e095d0
josephwehby <josephw31@hotmail.com>
2023-06-22 10:33:00 -0500

    finally fixed?

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Level.cpp.o"
+  "CMakeFiles/main.dir/src/Level.cpp.o.d"
+  "CMakeFiles/main.dir/src/Order.cpp.o"
+  "CMakeFiles/main.dir/src/Order.cpp.o.d"
+  "CMakeFiles/main.dir/src/OrderBook.cpp.o"
+  "CMakeFiles/main.dir/src/OrderBook.cpp.o.d"
+  "CMakeFiles/main.dir/src/User.cpp.o"
+  "CMakeFiles/main.dir/src/User.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "bin/main"
+  "bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
