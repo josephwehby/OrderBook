d792c18b9bf39adfbf6beab9d075fd30052bd27f
josephwehby <josephw31@hotmail.com>
2023-07-05 11:40:22 -0500

    added while loop to add orders

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,48 @@
 #include "OrderBook.hpp"
 #include "Order.hpp"
 #include <memory>
+#include <string>
 
 int main() {
 
     OrderBook orderbook;
-    std::shared_ptr<Order> bid;
-    std::shared_ptr<Order> bid2;
-    std::shared_ptr<Order> ask;
-    std::shared_ptr<Order> ask2;
+    std::shared_ptr<Order> order;
+    std::string buy_sell;
+    double price;
+    unsigned int quantity;
     
-    bid = std::make_shared<Order>(Buy, 100, 3.99);
-    bid2 = std::make_shared<Order>(Buy, 100, 4.30);
-    ask = std::make_shared<Order>(Sell, 100, 4.30);
-    ask2 = std::make_shared<Order>(Sell, 5000, 4.30);
-    
-	std::cout << "Limit Order Book" << std::endl;
+    std::cout << "Limit Order Book" << std::endl;
+    while (true) {
+        std::cout << "Buy/Sell: ";
+        std::cin >> buy_sell; 
+        
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        
+        std::cout << "Price: ";
+        std::cin >> price;
+        
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        
+        std::cout << "Quantity: ";
+        std::cin >> quantity;
 
-    orderbook.addOrder(bid);
-    orderbook.addOrder(ask);
-    orderbook.addOrder(ask2);
-    
-	orderbook.printOrderBook();
-    std::cout << "==================" << std::endl;
-	orderbook.addOrder(bid2);
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
 
-    orderbook.printOrderBook();
-    bid2 = std::make_shared<Order>(Buy, 1000, 4.32);
-    std::cout << "==================" << std::endl;
-	orderbook.addOrder(bid2);
-    orderbook.addOrder(bid2);
-	orderbook.printOrderBook();
-	
+        if (buy_sell == "buy") {
+            order = std::make_shared<Order>(Buy, quantity, price);
+        } else {
+            order = std::make_shared<Order>(Sell, quantity, price);
+        }
+
+        orderbook.addOrder(order);
+        orderbook.printOrderBook();
+        std::cout << "==================" << std::endl;
+
+
+    }
+
     return 0;
 }
