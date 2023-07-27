85dde42fe713e496e2da0daa2b16bd373d6464fa
josephwehby <josephw31@hotmail.com>
2023-07-27 12:19:20 -0500

    removed input from order book. preparing to only accept orders over network

--- include/Main.hpp
+++ include/Main.hpp
@@ -7,26 +7,15 @@ class Main : public wxFrame{
 public:
     Main();
 	~Main();
-    void onButtonClick(OrderType);
-    wxDECLARE_EVENT_TABLE();
-
-
+    // void onButtonClick(OrderType);
 private:
     void resetOrderBook();
-    void onBuyClick(wxCommandEvent &evt);
-    void onAskClick(wxCommandEvent &evt);
     
     OrderBook orderbook;    
-    wxStaticText* price_text = nullptr;
-    wxStaticText* quantity_text = nullptr;
     wxStaticText* mid_price = nullptr;
 	wxStaticText* spread = nullptr;
     wxStaticText* bid = nullptr;
 	wxStaticText* ask = nullptr;
-    wxButton* buy = nullptr;
-    wxButton* sell = nullptr;
-    wxTextCtrl* price = nullptr;
-    wxTextCtrl* quantity = nullptr;
     wxListBox* bidbox = nullptr;
     wxListBox* askbox = nullptr;
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,55 +1,27 @@
 #include "Main.hpp"
 
-wxBEGIN_EVENT_TABLE(Main, wxFrame)
-    EVT_BUTTON(10002, Main::onBuyClick)
-    EVT_BUTTON(10003, Main::onAskClick)
-wxEND_EVENT_TABLE()
-
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Limit Order Book", wxPoint(30, 30), wxSize(700, 455)) {
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Limit Order Book", wxPoint(30, 30), wxSize(380, 400)) {
 	
     wxFont font(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
-    
-    price_text = new wxStaticText(this, wxID_ANY, "Price", wxPoint(10, 10), wxSize(50,30));
-    price = new wxTextCtrl(this, 10000, "", wxPoint(80, 10), wxSize(130, 30));
 
-    quantity_text = new wxStaticText(this, wxID_ANY, "Qty", wxPoint(10, 50), wxSize(50,30));
-    quantity = new wxTextCtrl(this, 10001, "", wxPoint(80, 50), wxSize(130, 30));
-    
-    buy = new wxButton(this, 10002, "Buy", wxPoint(10, 100), wxSize(95,40));
-    buy->SetBackgroundColour(wxColour(*wxGREEN));
-    buy->SetFont(font);
-
-    sell = new wxButton(this, 10003, "Sell", wxPoint(115, 100), wxSize(95,40));
-    sell->SetBackgroundColour(wxColour(*wxRED));
-    sell->SetFont(font);
-
-    mid_price = new wxStaticText(this, wxID_ANY, "0.00", wxPoint(430, 10), wxSize(50,30));
-    spread = new wxStaticText(this, wxID_ANY, "0.00", wxPoint(430, 40), wxSize(50,30));
+    mid_price = new wxStaticText(this, wxID_ANY, "0.00", wxPoint(140, 10), wxSize(50,30));
+    spread = new wxStaticText(this, wxID_ANY, "0.00", wxPoint(140, 40), wxSize(50,30));
     mid_price->SetFont(font);
     spread->SetFont(font);
-    bid = new wxStaticText(this, wxID_ANY, "Bid", wxPoint(360, 80), wxSize(50,30));
+    
+    bid = new wxStaticText(this, wxID_ANY, "Bid", wxPoint(70, 80), wxSize(50,30));
     bid->SetFont(font);
-    bidbox = new wxListBox(this, 10004, wxPoint(300, 100), wxSize(150, 200));
+    bidbox = new wxListBox(this, 10004, wxPoint(10, 100), wxSize(150, 200));
     bidbox->SetForegroundColour(wxColour(*wxGREEN));
     
-    ask = new wxStaticText(this, wxID_ANY, "Ask", wxPoint(510, 80), wxSize(50,30));
+    ask = new wxStaticText(this, wxID_ANY, "Ask", wxPoint(230, 80), wxSize(50,30));
     ask->SetFont(font);
-    askbox = new wxListBox(this, 10005, wxPoint(450, 100), wxSize(150, 200));
+    askbox = new wxListBox(this, 10005, wxPoint(170, 100), wxSize(150, 200));
     askbox->SetForegroundColour(wxColour(*wxRED));
 }
 
 Main::~Main() {
 
-}
-
-void Main::onBuyClick(wxCommandEvent &evt) {
-    onButtonClick(Buy);
-    evt.Skip();
-}
-
-void Main::onAskClick(wxCommandEvent &evt) {
-    onButtonClick(Sell);
-    evt.Skip();
 }
 
 void Main::resetOrderBook() {
@@ -70,7 +42,7 @@ void Main::resetOrderBook() {
     spread->SetLabel(wxString(str_spread));
     mid_price->SetLabel(wxString(str_mid));
 }
-
+/*
 void Main::onButtonClick(OrderType order_type) {
     std::shared_ptr<Order> order;
     double share_price;
@@ -90,3 +62,4 @@ void Main::onButtonClick(OrderType order_type) {
     
     resetOrderBook();
 }
+*/
